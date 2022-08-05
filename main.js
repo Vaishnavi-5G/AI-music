863470066b85e901dfdfee64781ea2cc1d6eb179
Vaishnavi-5G <96188244+Vaishnavi-5G@users.noreply.github.com>
2022-08-05 19:03:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song1 = "";
+song2 = "";
+
+function preload()
+{
+    song1 = loadSound("chand baliya.mp3");
+    song2 = loadSound("pasoori.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+ 
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
